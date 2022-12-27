e91386f48c6a57c224599f8a49727c8f4c29fdab
unohayato <mamidori1p1p@gmail.com>
2022-12-27 21:56:54 +0900

    reset

--- config/urls.py
+++ config/urls.py
@@ -15,15 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
 
-index_view = TemplateView.as_view(template_name='registration/index.html')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_required(index_view), name='index'),
-    path('', include('django.contrib.auth.urls')),
+  
    
 ]
