764044d79c44f08b335d846884bb89fbc85d8528
unohayato <mamidori1p1p@gmail.com>
2022-12-27 22:04:10 +0900

    ok

--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+EMAIL_BACKEND = 'django.core.email.backends.console.EmailBackend'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/login/'
