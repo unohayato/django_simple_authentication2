a782c4fa1f10e237c72fbd14c118d18a1a10eedc
unohayato <mamidori1p1p@gmail.com>
2023-01-01 21:49:15 +0900

    reset

--- registration/models.py
+++ registration/models.py
@@ -1,8 +1,2 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
-class User(AbstractUser):
-  email = models.EmailField('メールアドレス', unique=True)
   
