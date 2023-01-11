a57a65f77b8a7f19e202892ae12a056fc04d51fd
unohayato <mamidori1p1p@gmail.com>
2023-01-11 17:48:02 +0900

    forms.py

---
+++ registration/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.conf import settings
+
+User = get_user_model()
+
+class SignUpForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ("username", "email", "password1", "password2")
+    
+  def save(self, commit=True):
+    user = super().save(commit=False)
+    user.email = self.cleaned_data["email"]
+    user.save()
+    return user
