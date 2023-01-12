4fc1fd196368d96a67b015dbeb4b63ebfd4a7a8c
unohayato <mamidori1p1p@gmail.com>
2023-01-12 20:47:06 +0900

    views.py

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import SignUpform
+
+class SognUpView(CreateView):
+  form_class = SignUpform
+  success_url = reverse_lazy('login')
+  template_name = 'registration/signup.html'
+  
