f183d188d5af880e14329c57bd623c6954442d95
unohayato <mamidori1p1p@gmail.com>
2023-01-14 21:50:17 +0900

    urls

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
+from registration import views
 
 index_view = TemplateView.as_view(template_name='registration/index.html')
 
@@ -25,5 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login_required(index_view), name='index'),
     path('', include('django.contrib.auth.urls')),
+    path('signup/', views.SognUpView.as_view(), name="signup")
    
 ]
