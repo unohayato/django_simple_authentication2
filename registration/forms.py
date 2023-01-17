574cfa4dda541c7395bb493b984fe3da8bd3c94a
unohayato <mamidori1p1p@gmail.com>
2023-01-17 21:09:10 +0900

    forms.py import

--- registration/forms.py
+++ registration/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.conf import settings
 
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
 User = get_user_model()
 
 class SignUpForm(UserCreationForm):
